38060520c7725677ef9dc4b14d9cd9fb96b1ed8a
phillsatellite <pprints12@gmail.com>
2025-06-12 23:58:28 -0400

    inventoryManagement Lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,42 @@
-// Write your code here
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
+//Function logs the first product within the products array
+function logFirstProduct(){
+  const firstProduct = products[0];
+  console.log(firstProduct);
+}
 
+logFirstProduct(); //Should show Laptop for index 0
+
+//Adds new product to the product array 
+function addProduct(name){
+  const newProduct = {
+    name: name
+  }
+  products.push(newProduct);
+}
+
+addProduct("keyboard");
+//Should show array wiht Keyboard added as well
+console.log(products);
+
+//Updates the name of the first index/product in the products array
+function updateProductName(newName){
+  products[0] = newName;
+}
+
+updateProductName("Macbook");
+//"Macbook" should appear for index 0
+console.log(products); 
+
+//Removes the last index(product) in the array(products)
+function removeLastProduct(){
+  products.pop();
+}
+
+removeLastProduct(products);
+//"Keyboard should no longer appear in the array(products)
+console.log(products);
 
 // Export the necessary parts for testing
 module.exports = {
